ef5d137d42b4856eee62c60764b6ddfcc1ba7eb3
Alexander A. Dyshev <adyshev@gmail.com>
2024-05-15 18:42:24 +0300

    Refactoring

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,3 @@
 return {
   { "nvimdev/dashboard-nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
---
+++ .config/nvim/lua/plugins/filetree.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        width = 40,
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".git",
+          },
+          never_show = {},
+        },
+      },
+    })
+  end,
+}
