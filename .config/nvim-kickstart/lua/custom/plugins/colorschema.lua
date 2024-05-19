c41de8f4e0bfe45fdd005ded0a21fbc442685ff9
Alexander A. Dyshev <adyshev@gmail.com>
2024-05-19 18:14:30 +0300

    Refactoring

---
+++ .config/nvim-kickstart/lua/custom/plugins/colorschema.lua
@@ -0,0 +1,34 @@
+return {
+  -- {
+  --   'navarasu/onedark.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('onedark').setup {
+  --       style = 'warm', -- or cool | dark | darker
+  --     }
+  --     require('onedark').load()
+  --     vim.cmd.colorscheme 'onedark'
+  --   end,
+  -- },
+
+  {
+    'f4z3r/gruvbox-material.nvim',
+    name = 'gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require('gruvbox-material').setup {
+        italics = true,
+        comments = {
+          italics = true,
+        },
+        contrast = 'medium',
+      }
+    end,
+  },
+
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- { 'wittyjudge/gruvbox-material.nvim' },
+}
