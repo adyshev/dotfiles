2a7609c860e1263959de3830702aadb5340da95c
Alexander A. Dyshev <adyshev@gmail.com>
2024-05-22 19:59:43 +0300

    Refactoring

--- .config/nvim-kickstart/lua/custom/plugins/noneckpain.lua
+++ .config/nvim-kickstart/lua/custom/plugins/noneckpain.lua
@@ -1,13 +1,10 @@
 return {
-  -- add symbols-outline
   'shortcuts/no-neck-pain.nvim',
   cmd = 'NoNeckPain',
   keys = { { '<leader>z', '<cmd>NoNeckPain<cr>', desc = '[z]Zen Mode' } },
   config = function()
     require('no-neck-pain').setup {
       width = 120,
-      disableOnLastBuffer = true,
-      killAllBuffersOnDisable = true,
     }
   end,
 }
