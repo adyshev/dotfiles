e7545e48da582979641a5c97334a3365be4f128d
Alexander A. Dyshev <adyshev@gmail.com>
2024-05-22 20:29:21 +0300

    Refactoring

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -831,7 +831,24 @@ require('lazy').setup({
   -- },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    config = function()
+      require('todo-comments').setup { signs = false }
+      vim.keymap.set('n', ']t', function()
+        require('todo-comments').jump_next()
+        -- require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
+      end, { desc = 'Next todo comment' })
+
+      vim.keymap.set('n', '[t', function()
+        require('todo-comments').jump_prev()
+      end, { desc = 'Previous todo comment' })
+    end,
+
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {},
+  },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
