3040a7cc64a8a782630a90a45743a106d08a0b23
Alexander A. Dyshev <adyshev@gmail.com>
2024-05-23 14:00:17 +0300

    Refactoring

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -780,7 +780,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-Space>'] = cmp.mapping.confirm { select = true },
+          ['<Space>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
