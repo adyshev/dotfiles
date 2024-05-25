f1f2063a56f93d0666408f7116b8761ab7d6c299
Alexander A. Dyshev <adyshev@gmail.com>
2024-05-25 14:38:49 +0300

    Refactoring

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -982,47 +982,103 @@ require('lazy').setup({
   {
     'b0o/incline.nvim',
     config = function()
-      local function get_diagnostic_label(props)
-        local icons = {
-          Error = '',
-          Warn = '',
-          Info = '',
-          Hint = '',
-        }
-
-        local label = {}
-        for severity, icon in pairs(icons) do
-          local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-          if n > 0 then
-            local fg = '#' .. string.format('%06x', vim.api.nvim_get_hl_by_name('DiagnosticSign' .. severity, true)['foreground'])
-            table.insert(label, { icon .. ' ' .. n .. ' ', guifg = fg })
-          end
-        end
-        return label
-      end
-
       require('incline').setup {
-        debounce_threshold = { falling = 500, rising = 250 },
+        window = {
+          placement = {
+            vertical = 'bottom',
+            horizontal = 'center',
+          },
+          padding = 0,
+          margin = { vertical = 0, horizontal = 0 },
+        },
+        hide = {
+          cursorline = true,
+        },
         render = function(props)
-          local bufname = vim.api.nvim_buf_get_name(props.buf)
-          local filename = vim.fn.fnamemodify(bufname, ':t')
-          local diagnostics = get_diagnostic_label(props)
-          local modified = vim.api.nvim_buf_get_option(props.buf, 'modified') and 'bold,italic' or 'None'
-          local filetype_icon, color = require('nvim-web-devicons').get_icon_color(filename)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+          if filename == '' then
+            filename = '[No Name]'
+          end
+          local ft_icon, ft_color = require('nvim-web-devicons').get_icon_color(filename)
 
-          local buffer = {
-            { filetype_icon, guifg = color },
-            { ' ' },
-            { filename, gui = modified },
+          local function get_git_diff()
+            local icons = { removed = ' ', changed = ' ', added = ' ' }
+            local signs = vim.b[props.buf].gitsigns_status_dict
+            local labels = {}
+            if signs == nil then
+              return labels
+            end
+            for name, icon in pairs(icons) do
+              if tonumber(signs[name]) and signs[name] > 0 then
+                table.insert(labels, { icon .. signs[name] .. ' ', group = 'Diff' .. name })
+              end
+            end
+            if #labels > 0 then
+              table.insert(labels, { '| ' })
+            end
+            return labels
+          end
+
+          local function get_diagnostic_label()
+            local icons = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+            local label = {}
+
+            for severity, icon in pairs(icons) do
+              local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+              if n > 0 then
+                table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+              end
+            end
+            if #label > 0 then
+              table.insert(label, { '| ' })
+            end
+            return label
+          end
+
+          local function get_harpoon_items()
+            local harpoon = require 'harpoon'
+            local marks = harpoon:list().items
+            local current_file_path = vim.fn.expand '%:p:.'
+            local label = {}
+
+            for id, item in ipairs(marks) do
+              if item.value == current_file_path then
+                table.insert(label, { id .. ' ', guifg = '#FFFFFF', gui = 'bold' })
+              else
+                table.insert(label, { id .. ' ', guifg = '#434852' })
+              end
+            end
+
+            if #label > 0 then
+              table.insert(label, 1, { '󰛢 ', guifg = '#61AfEf' })
+              table.insert(label, { '| ' })
+            end
+            return label
+          end
+
+          local function get_file_name()
+            local label = {}
+            table.insert(label, { (ft_icon or '') .. ' ', guifg = ft_color, guibg = 'none' })
+            table.insert(label, { vim.bo[props.buf].modified and ' ' or '', guifg = '#d19a66' })
+            table.insert(label, { filename, gui = vim.bo[props.buf].modified and 'bold,italic' or 'bold' })
+            if not props.focused then
+              label['group'] = 'BufferInactive'
+            end
+
+            return label
+          end
+
+          return {
+            { '', guifg = '#282828', guibg = '#282828' },
+            {
+              { get_diagnostic_label() },
+              { get_git_diff() },
+              { get_harpoon_items() },
+              { get_file_name() },
+              guibg = '#282828',
+            },
+            { '', guifg = '#282828', guibg = '#282828' },
           }
-
-          if #diagnostics > 0 then
-            table.insert(diagnostics, { '| ', guifg = 'grey' })
-          end
-          for _, buffer_ in ipairs(buffer) do
-            table.insert(diagnostics, buffer_)
-          end
-          return diagnostics
         end,
       }
     end,
--- .config/nvim-kickstart/lua/custom/autocmds.lua
+++ .config/nvim-kickstart/lua/custom/autocmds.lua
@@ -19,6 +19,36 @@ vim.api.nvim_create_autocmd('FocusLost', {
   command = 'silent! wall',
 })
 
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Jump to the last known position of a file before closing it',
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
+
+    if mark[1] > 0 and mark[1] <= line_count then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  desc = "Close some filtypes simply by pressing 'q'",
+  pattern = { 'checkhealth', 'help', 'lspinfo', 'man', 'notify', 'qf', 'query' },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+  desc = 'Redraw the cursorline when navigating around the buffer',
+  callback = function()
+    if vim.wo.cursorline then
+      vim.cmd 'redraw'
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd('VimEnter', {
   desc = 'Auto select virtualenv Nvim open',
   pattern = '*',
