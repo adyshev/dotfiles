f4994d2c0196b76bcd54d2206fe4a511d774f6d7
Alexander A. Dyshev <adyshev@gmail.com>
2024-05-27 11:33:36 +0300

    Refactoring

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -972,12 +972,32 @@ require('lazy').setup({
   },
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'archibate/lualine-time' },
     config = function()
       require('lualine').setup {
         options = {
           theme = 'gruvbox-material',
         },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'cdate', 'ctime' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
       }
     end,
   },
