0a9495714f46ec4d809481e7914936989b23bdf0
Alexander A. Dyshev <adyshev@gmail.com>
2024-06-16 12:14:19 +0300

    Float terminal

--- nvim/.config/nvim-kickstart/lua/custom/options.lua
+++ nvim/.config/nvim-kickstart/lua/custom/options.lua
@@ -30,3 +30,10 @@ vim.o.spellfile = os.getenv 'HOME' .. '/.config/nvim-kickstart/spell/en.utf-8.ad
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Not sure
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrw_gitignore = 1
