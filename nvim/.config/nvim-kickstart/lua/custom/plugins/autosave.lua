59ee084c740f1ad892537fba52e5fb5f1ea9fc93
Oleksandr Dyshev <odyshev@lyft.com>
2024-07-18 10:53:56 +0300

    Adding auto-save plugin

--- nvim/.config/nvim-kickstart/lua/custom/plugins/autosave.lua
+++ nvim/.config/nvim-kickstart/lua/custom/plugins/autosave.lua
@@ -10,6 +10,15 @@ return {
           return ''
         end,
       },
+      condition = function(buf)
+        local fn = vim.fn
+
+        -- don't save for special-buffers
+        if fn.getbufvar(buf, '&buftype') ~= '' then
+          return false
+        end
+        return true
+      end,
       trigger_events = { -- See :h events
         immediate_save = { 'BufLeave', 'FocusLost' }, -- vim events that trigger an immediate save
         defer_save = {}, -- vim events that trigger a deferred save (saves after `debounce_delay`)
