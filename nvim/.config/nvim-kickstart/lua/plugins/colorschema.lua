ba6f05ca8b577f078e43471b42ce278231256d09
Alexander A. Dyshev <adyshev@gmail.com>
2024-08-10 10:11:37 +0300

    Introducing noice & lots of fundamental UI changes

--- nvim/.config/nvim-kickstart/lua/plugins/colorschema.lua
+++ nvim/.config/nvim-kickstart/lua/plugins/colorschema.lua
@@ -12,38 +12,77 @@ return {
   --   end,
   -- },
   {
-    'f4z3r/gruvbox-material.nvim',
-    name = 'gruvbox-material',
+    'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000,
-    opts = {},
     config = function()
-      local colors = require('gruvbox-material.colors').get(vim.o.background, 'medium')
-
-      require('gruvbox-material').setup {
-        italics = true,
-        background = {
-          transparent = false, -- set the background to transparent
-        },
-        float = {
-          force_background = false, -- force background on floats even when background.transparent is set
-          background_color = '#282828', -- set color for float backgrounds. If nil, uses the default color set
-        },
-        comments = {
-          italics = true,
-        },
-        contrast = 'medium',
-        customize = function(g, o)
-          if g == 'CursorLineNr' then
-            o.link = nil -- wipe a potential link, which would take precedence over other
-            -- attributes
-            o.fg = colors.orange -- or use any color in "#rrggbb" hex format
-          end
-          o.bold = false
-          return o
-        end,
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_enable_bold = 0
+      vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+      vim.g.gruvbox_material_dim_inactive_windows = 1
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_background = 'medium'
+      vim.g.gruvbox_material_foreground = 'mix' -- 'material'`, `'mix'`, `'original
+      vim.g.gruvbox_material_ui_contrast = 'high' -- `'low'`, `'high'`
+      vim.g.gruvbox_material_float_style = 'dim'
+      vim.g.gruvbox_material_show_eob = 1
+      vim.g.gruvbox_material_diagnostic_line_highlight = 0
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_colors_override = {
+        bg0 = { '#282828', '235' },
+        bg3 = { '#484545', '237' },
+        bg_dim = { '#282828', '235' },
       }
+
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        group = vim.api.nvim_create_augroup('custom_highlights_gruvboxmaterial', {}),
+        pattern = 'gruvbox-material',
+        callback = function()
+          local config = vim.fn['gruvbox_material#get_configuration']()
+          local palette = vim.fn['gruvbox_material#get_palette'](config.background, config.foreground, config.colors_override)
+          local set_hl = vim.fn['gruvbox_material#highlight']
+
+          set_hl('CursorLineNr', palette.orange, palette.none)
+        end,
+      })
+
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
+  -- {
+  --   'f4z3r/gruvbox-material.nvim',
+  --   name = 'gruvbox-material',
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     local colors = require('gruvbox-material.colors').get(vim.o.background, 'medium')
+  --
+  --     require('gruvbox-material').setup {
+  --       italics = true,
+  --       background = {
+  --         transparent = false, -- set the background to transparent
+  --       },
+  --       float = {
+  --         force_background = false, -- force background on floats even when background.transparent is set
+  --         background_color = '#282828', -- set color for float backgrounds. If nil, uses the default color set
+  --       },
+  --       comments = {
+  --         italics = true,
+  --       },
+  --       contrast = 'medium',
+  --       customize = function(g, o)
+  --         if g == 'CursorLineNr' then
+  --           o.link = nil -- wipe a potential link, which would take precedence over other
+  --           -- attributes
+  --           o.fg = colors.orange -- or use any color in "#rrggbb" hex format
+  --         end
+  --
+  --         o.bold = false
+  --         return o
+  --       end,
+  --     }
+  --   end,
+  -- },
   {
     'norcalli/nvim-colorizer.lua',
     config = function()
---
+++ nvim/.config/nvim-kickstart/lua/plugins/noice.lua
@@ -0,0 +1,39 @@
+return {
+  'folke/noice.nvim',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
+  },
+  opts = {
+    lsp = {
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
+      },
+    },
+    presets = {
+      bottom_search = false,
+      command_palette = false,
+      long_message_to_split = true,
+      inc_rename = true,
+      lsp_doc_border = true,
+    },
+    routes = {
+      {
+        filter = {
+          event = 'msg_show',
+          any = {
+            { find = '%d+L, %d+B' },
+            { find = '; after #%d+' },
+            { find = '; before #%d+' },
+          },
+        },
+        view = 'mini',
+      },
+    },
+  },
+  config = function(_, opts)
+    require('noice').setup(opts)
+  end,
+}
