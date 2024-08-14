38bd3b1bce02f94750f66602f9a35c0ccc6c1dd6
Alexander A. Dyshev <adyshev@gmail.com>
2024-08-14 23:58:04 +0300

    refactoring neorg

--- nvim/.config/nvim-kickstart/after/ftplugin/go.lua
+++ nvim/.config/nvim-kickstart/after/ftplugin/go.lua
@@ -1,4 +1,16 @@
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = false
+vim.opt_local.listchars = 'tab:  '
+
+local goaugroup = vim.api.nvim_create_augroup('goformat', { clear = true })
+
+-- format and organize imports on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = goaugroup,
+  pattern = '*.go',
+  callback = function()
+    vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' } }, apply = true }
+  end,
+})
--- nvim/.config/nvim-kickstart/lua/plugins/neorg.lua
+++ nvim/.config/nvim-kickstart/lua/plugins/neorg.lua
@@ -1,15 +1,13 @@
 local neorg_dependencies = {
   'nvim-treesitter/nvim-treesitter',
   'hrsh7th/nvim-cmp',
-  'nvim-neorg/neorg-telescope',
 }
 
 local lazy_loading_keymaps = {
-  { '<leader>nt', '<cmd>Neorg journal today<cr>', desc = "[t]Today's Journal Page" },
-  { '<leader>nw', '<cmd>Neorg workspace work<cr>', desc = '[w]Edit Work Notes' },
-  { '<leader>np', '<cmd>Neorg workspace personal<cr>', desc = '[p]Edit Personal Notes' },
-  { '<leader>ns', '<cmd>Neorg generate-workspace-summary<cr>', desc = '[s]Generate Workspace Summary' },
-  { '<leader>nf', '<cmd>Telescope neorg search_headings<CR>', desc = '[f]Find Headings' },
+  { '<leader>nj', '<cmd>Neorg journal today<cr>', desc = '[j]Journal for Today' },
+  { '<leader>nw', '<cmd>Neorg workspace work<cr>', desc = '[w]Work Notes' },
+  { '<leader>np', '<cmd>Neorg workspace personal<cr>', desc = '[p]Personal Notes' },
+  { '<leader>ne', '<cmd>Neorg export<cr>', desc = '[e]Export Notes' },
 }
 
 local dirman_configuration = {
@@ -27,7 +25,6 @@ local dirman_configuration = {
 
 local module_list = {
   ['core.defaults'] = {},
-  ['core.concealer'] = {},
   ['core.dirman'] = dirman_configuration,
   ['core.presenter'] = {
     config = {
@@ -39,8 +36,6 @@ local module_list = {
       engine = 'nvim-cmp',
     },
   },
-  ['core.keybinds'] = {},
-  ['core.qol.toc'] = {},
   ['core.journal'] = {
     config = {
       journal_folder = '/years/',
@@ -51,17 +46,74 @@ local module_list = {
   ['core.esupports.metagen'] = {
     config = {
       type = 'auto',
+      update_date = true,
+      tab = '  ',
+      template = {
+        -- The title field generates a title for the file based on the filename.
+        {
+          'title',
+          function()
+            return vim.fn.expand '%:p:t:r'
+          end,
+        },
+
+        { 'description', '' },
+        { 'authors', 'Oleksandr Dyshev' },
+        {
+          'categories',
+          '',
+        },
+
+        -- The created field is populated with the current date as returned by `os.date`.
+        {
+          'created',
+          function()
+            return os.date '%Y-%m-%d'
+          end,
+        },
+
+        -- When creating fresh, new metadata, the updated field is populated the same way
+        -- as the `created` date.
+        {
+          'updated',
+          function()
+            return os.date '%Y-%m-%d'
+          end,
+        },
+
+        -- The version field determines which Norg version was used when
+        -- the file was created.
+        { 'version', require('neorg.core.config').version },
+      },
     },
   },
-  ['core.export'] = {},
+  ['core.export'] = {
+    config = {
+      export_dir = '~/neorg-export/<language>',
+    },
+  },
   ['core.export.markdown'] = {
     config = {
       extensions = 'all',
     },
   },
-  ['core.integrations.telescope'] = {},
   ['core.summary'] = {},
-  ['core.ui.calendar'] = {},
+  ['core.integrations.nvim-cmp'] = {},
+  ['core.concealer'] = {
+    config = {
+      folds = false,
+      icon_preset = 'diamond',
+      icons = {
+        todo = {
+          undone = {
+            enabled = true,
+            icon = ' ',
+            query = '(todo_item_undone) @icon',
+          },
+        },
+      },
+    },
+  },
 }
 
 return {
