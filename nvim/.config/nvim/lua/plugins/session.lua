2000ce4d87e6e8cfaf11668dd8527f1d1ee8397f
Alexander A. Dyshev <adyshev@gmail.com>
2024-09-06 14:57:56 +0300

    update resession.nvim integration

--- nvim/.config/nvim/lua/plugins/session.lua
+++ nvim/.config/nvim/lua/plugins/session.lua
@@ -2,17 +2,7 @@ return {
   'stevearc/resession.nvim',
   config = function()
     local resession = require 'resession'
-    local session_dir = vim.fn.stdpath 'config' .. '/session'
-
-    resession.setup {
-      dir = session_dir,
-    }
-
-    vim.api.nvim_create_autocmd('VimLeavePre', {
-      callback = function()
-        resession.save 'last'
-      end,
-    })
+    resession.setup {}
 
     vim.keymap.set('n', '<leader>ss', resession.save, { desc = '[s]Save Session' })
     vim.keymap.set('n', '<leader>sl', resession.load, { desc = '[l]Load Session' })
