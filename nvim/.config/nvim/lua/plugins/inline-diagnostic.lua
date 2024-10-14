5824087a52ab051c1a4101f09310bce0d3559a31
Alexander A. Dyshev <adyshev@gmail.com>
2024-10-14 11:03:52 +0300

    tuning inline diagnostic messages

--- nvim/.config/nvim/lua/plugins/inline-diagnostic.lua
+++ nvim/.config/nvim/lua/plugins/inline-diagnostic.lua
@@ -7,13 +7,25 @@ return {
         arrow = 'CursorLineNr',
         mixing_color = '#3A3A3A',
       },
+      options = {
+        throttle = 0,
+
+        -- The minimum length of the message, otherwise it will be on a new line.
+        softwrap = 30,
+
+        -- If multiple diagnostics are under the cursor, display all of them.
+        multiple_diag_under_cursor = true,
+
+        -- Enable diagnostic message on all lines.
+        multilines = true,
+      },
       blend = {
         factor = 0,
       },
       signs = {
         left = ' ',
-        right = '',
-        diag = '',
+        right = ' ',
+        diag = ' ⚠',
         arrow = '    ',
         up_arrow = '    ',
         vertical = '',
