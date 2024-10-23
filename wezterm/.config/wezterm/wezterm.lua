58f02beae41d222ef91f1fe6b532b6c35cb74e09
Oleksandr Dyshev <odyshev@lyft.com>
2024-10-23 18:00:31 +0300

    Fix minor bugs in configuration

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+local mux = wezterm.mux
 local config = {}
 
 if wezterm.config_builder then
@@ -34,6 +35,45 @@ config.pane_focus_follows_mouse = true
 config.use_fancy_tab_bar = true
 config.switch_to_last_active_tab_when_closing_tab = true
 config.default_cursor_style = "SteadyBar"
+
+config.window_frame = {
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 12.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#282828",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#282828",
+}
+
+config.colors = {
+	tab_bar = {
+		background = "#282828",
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#282828",
+		active_tab = {
+			-- The color of the background area for the tab
+			bg_color = "#32302F",
+			fg_color = "#E2CCA9",
+		},
+		inactive_tab = {
+			bg_color = "#282828",
+			fg_color = "#5A524C",
+		},
+	},
+}
 
 -- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
 local function is_vim(pane)
