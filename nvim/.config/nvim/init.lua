88334dde62b6eacfb55526f306488c702341dd5a
Alexander A. Dyshev <adyshev@gmail.com>
2025-04-07 19:08:41 +0300

    Refactoring nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -727,7 +727,28 @@ require("lazy").setup({
             -- require('mini.surround').setup()
             require("mini.operators").setup()
             require("mini.trailspace").setup()
-            require("mini.move").setup()
+            require("mini.move").setup({
+                -- Module mappings. Use `''` (empty string) to disable one.
+                mappings = {
+                    -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+                    left = "<C-h>",
+                    right = "<C-l>",
+                    down = "<C-j>",
+                    up = "<C-k>",
+
+                    -- Move current line in Normal mode
+                    line_left = "<C-h>",
+                    line_right = "<C-l>",
+                    line_down = "<C-j>",
+                    line_up = "<C-k>",
+                },
+
+                -- Options which control moving behavior
+                options = {
+                    -- Automatically reindent selection during linewise vertical move
+                    reindent_linewise = false,
+                },
+            })
             -- require('mini.indentscope').setup {
             --   symbol = '│',
             --   options = {
