a6549447a849e5162ce9acd24b60bbe0a8240c1e
Oleksandr Dyshev <odyshev@lyft.com>
2025-08-07 11:04:51 +0300

    Update venv-selector config -> required by newest version

--- nvim/.config/nvim/lua/plugins/venv-selector.lua
+++ nvim/.config/nvim/lua/plugins/venv-selector.lua
@@ -1,17 +1,14 @@
 return {
     "linux-cultist/venv-selector.nvim",
-    enabled = true,
-    dependencies = {
-        "neovim/nvim-lspconfig",
-        "mfussenegger/nvim-dap",
-        "mfussenegger/nvim-dap-python", --optional
-        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-    },
-    lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     config = function()
-        require("venv-selector").setup()
+        require("venv-selector").setup({
+            -- Your options go here
+            -- name = "venv",
+            -- auto_refresh = false
+        })
     end,
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     keys = {
         { "<leader>v", "<cmd>VenvSelect<cr>", desc = "[v]Venv Selector" },
     },
