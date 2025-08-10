7ceb740b93cd9ae49ea8dfe5eb56ef0798c7ada1
Oleksandr Dyshev <odyshev@lyft.com>
2025-08-10 12:45:05 +0300

    autcmd refactor

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -65,6 +65,18 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     desc = "Trim spaces",
 })
 
+vim.api.nvim_create_autocmd("VimEnter", {
+    desc = "Auto select virtualenv Nvim open",
+    pattern = "*",
+    callback = function()
+        local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+        if venv ~= "" then
+            require("venv-selector").retrieve_from_cache()
+        end
+    end,
+    once = true,
+})
+
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = { "*.py" },
     callback = function()
