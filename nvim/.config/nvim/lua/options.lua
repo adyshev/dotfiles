0af799c0198df3d55e72a4e7a2e4a6cdd664de00
Oleksandr Dyshev <odyshev@lyft.com>
2025-09-01 14:24:52 +0300

    update / optimize config

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,49 +1,51 @@
 local indent = 2
 
+vim.schedule(function()
+    vim.o.clipboard = "unnamedplus"
+end)
+
+vim.o.spellfile = os.getenv("HOME") .. "/.config/nvim/spell/en.utf-8.add"
 vim.o.formatoptions = "jcroqlnt"
 vim.o.shortmess = "filnxtToOFWIcC"
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.smartcase = true
+vim.o.smartindent = true
+vim.o.signcolumn = "yes"
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitbelow = true
+vim.o.splitkeep = "screen"
+vim.o.splitright = true
+vim.o.ignorecase = true
+vim.o.inccommand = "split"
+vim.o.list = true
+vim.o.confirm = true
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.mouse = "a"
+vim.o.showmode = false
 vim.opt.wrap = false
-vim.opt.breakindent = true
 vim.opt.cmdheight = 0
 vim.opt.completeopt = "menuone,noselect"
 vim.opt.conceallevel = 0
-vim.opt.confirm = true
-vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.hidden = true
 vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.inccommand = "nosplit"
 vim.opt.joinspaces = false
 vim.opt.laststatus = 0
-vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-vim.opt.mouse = "a"
-vim.opt.number = true
 vim.opt.pumblend = 10
 vim.opt.pumheight = 10
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 8
 vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 vim.opt.shiftround = true
 vim.opt.shiftwidth = indent
-vim.opt.showmode = false
 vim.opt.sidescrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.splitbelow = true
-vim.opt.splitkeep = "screen"
-vim.opt.splitright = true
 vim.opt.tabstop = indent
 vim.opt.termguicolors = true
-vim.opt.timeoutlen = 300
-vim.opt.undofile = true
-vim.opt.updatetime = 100
 vim.opt.wildmode = "longest:full,full"
-vim.opt.clipboard = "unnamedplus"
-
-vim.o.spellfile = os.getenv("HOME") .. "/.config/nvim/spell/en.utf-8.add"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
