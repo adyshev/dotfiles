8aa89ad9bde9af203ce86e344419c2ef9ae82006
Oleksandr Dyshev <odyshev@lyft.com>
2026-02-17 21:36:23 +0200

    refactor

--- nvim/.config/nvim/lua/plugins/todo.lua
+++ nvim/.config/nvim/lua/plugins/todo.lua
@@ -1,6 +1,6 @@
 local function _1_()
     local todo = require("floatingtodo")
-    todo.setup({ target_file = "~/notes/todo.md", width = 0.9, position = "center" })
+    todo.setup({ target_file = "~/notes/todo.md", width = 0.8, height = 0.6, border = "rounded", position = "bottomright" })
     return vim.keymap.set("n", "<leader>n", ":Td<CR>", { desc = "[n]Notes", silent = true })
 end
 return { "vimichael/floatingtodo.nvim", config = _1_ }
