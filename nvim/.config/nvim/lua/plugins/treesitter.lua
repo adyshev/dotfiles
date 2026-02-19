26404ec57b4b11552a68082b3f696bf43e9d3e9d
Oleksandr Dyshev (Олександр Андрійович Дишев) <odyshev@c7k6pywnjw.tailbaa43.ts.net>
2026-02-19 17:33:15 +0200

    fix

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,44 @@
+local parsers = {
+    "bash",
+    "c",
+    "css",
+    "diff",
+    "editorconfig",
+    "zsh",
+    "fish",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "go",
+    "html",
+    "javascript",
+    "jsdoc",
+    "json",
+    "latex",
+    "lua",
+    "luadoc",
+    "make",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "query",
+    "regex",
+    "rust",
+    "scss",
+    "svelte",
+    "templ",
+    "toml",
+    "tsx",
+    "typescript",
+    "typst",
+    "vim",
+    "vimdoc",
+    "xml",
+    "yaml",
+}
+
 return {
     {
         "nvim-treesitter/nvim-treesitter",
@@ -8,70 +49,27 @@ return {
             require("nvim-treesitter").setup({
                 install_dir = vim.fn.stdpath("data") .. "/site",
             })
-
-            local parsers = {
-                "bash",
-                "c",
-                "css",
-                "diff",
-                "editorconfig",
-                "zsh",
-                "git_config",
-                "git_rebase",
-                "gitattributes",
-                "gitcommit",
-                "gitignore",
-                "go",
-                "html",
-                "javascript",
-                "jsdoc",
-                "json",
-                "latex",
-                "lua",
-                "luadoc",
-                "make",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "rust",
-                "scss",
-                "svelte",
-                "templ",
-                "toml",
-                "tsx",
-                "typescript",
-                "typst",
-                "vim",
-                "vimdoc",
-                "xml",
-                "yaml",
-            }
-
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "LazyDone",
-                once = true,
-                callback = function()
-                    require("nvim-treesitter").install(parsers)
-                end,
-            })
-
-            require("treesitter-modules").setup({
-                auto_install = true,
-                incremental_selection = {
-                    enable = true,
-                    -- set value to `false` to disable individual mapping
-                    -- node_decremental captures both node_incremental and scope_incremental
-                    keymaps = {
-                        init_selection = "<c-space>",
-                        node_incremental = "<c-space>",
-                        scope_incremental = false,
-                        node_decremental = "<BS>",
-                    },
-                },
-            })
         end,
     },
-    { "MeanderingProgrammer/treesitter-modules.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" } },
+    {
+        "MeanderingProgrammer/treesitter-modules.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {
+            ensure_installed = parsers,
+            -- fold = { enable = true },
+            highlight = { enable = true },
+            -- indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                -- set value to `false` to disable individual mapping
+                -- node_decremental captures both node_incremental and scope_incremental
+                keymaps = {
+                    init_selection = "<c-space>",
+                    node_incremental = "<c-space>",
+                    scope_incremental = false,
+                    node_decremental = "<BS>",
+                },
+            },
+        },
+    },
 }
