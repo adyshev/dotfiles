5cafa19c899c6f26dc47cb3e56fc8ec1a31066b3
Oleksandr Dyshev <odyshev@lyft.com>
2026-03-13 09:27:56 +0200

    fixing multi-curosr bug

--- nvim/.config/nvim/lua/plugins/multicursor.lua
+++ nvim/.config/nvim/lua/plugins/multicursor.lua
@@ -5,9 +5,16 @@ return {
         vim.cmd([[
             let g:VM_default_mappings = 0
             let g:VM_maps = {}
-            let g:VM_maps['Find Under']         = '<C-n>'
-            let g:VM_maps['Find Subword Under'] = '<C-n>'
             let g:VM_theme = 'paper'
         ]])
+    end,
+    config = function()
+        -- Ensure v:hlsearch=1 before VM starts to prevent enable_hls() timer
+        -- from injecting feedkeys that corrupt the first 'n' press after restart.
+        vim.keymap.set("n", "<C-n>", function()
+            vim.v.hlsearch = 1
+            local key = vim.api.nvim_replace_termcodes("<Plug>(VM-Find-Under)", true, false, true)
+            vim.api.nvim_feedkeys(key, "m", false)
+        end, { noremap = false, silent = true, desc = "VM Find Under" })
     end,
 }
